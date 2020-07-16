a1e93ac0b7eaadeab8d6fe741e7c5955ff495da5
Mario Zorica <zorica.mario@rocketmail.com>
2020-07-16 13:33:58 +0200

    Test project that reproduces the IOException.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using System.Windows;
+using System.Windows.Xps.Packaging;
+
+namespace WpfApp1
+{
+    public partial class MainWindow : Window
+    {
+        private XpsDocument xps;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            /********************************************************************/
+            /* XPS document with picture and ReadWrite access reproduces issue. */
+            /********************************************************************/
+            this.xps = new XpsDocument("input.xps", FileAccess.ReadWrite);
+            this.viewer.Document = this.xps.GetFixedDocumentSequence();
+
+            /**********************************************************************/
+            /* XPS document with picture and Read access doesn't reproduce issue. */
+            /**********************************************************************/
+            //this.xps = new XpsDocument("input.xps", FileAccess.Read);
+            //this.viewer.Document = this.xps.GetFixedDocumentSequence();
+
+            /*********************************************************/
+            /* XPS document without picture doesn't reproduce issue. */
+            /*********************************************************/
+            //this.xps = new XpsDocument("inputWithoutPicture.xps", FileAccess.ReadWrite);
+            //this.viewer.Document = this.xps.GetFixedDocumentSequence();
+
+        }
+    }
+}
